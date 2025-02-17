2e8fb3cc32d9c2302b7107c128db39cad060d066
urvisurti897 <urvisurti897@gmail.com>
2025-02-16 19:48:52 -0500

    Defining class Customer and mentioning fields as private

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿namespace HQSP_Assignment_2_Task_1
 {
+    class Customer
+    {
+        private string name;
+        private string buildingType;
+        private int size;
+        private int lightBulbs;
+        private int outlets;
+        private string creditCard;
+    }
 
     internal class Program
     {
